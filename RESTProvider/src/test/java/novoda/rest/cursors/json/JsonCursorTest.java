11dc2eea93afcb20a1fba468e524a0f96b4cee93
Carl-Gustaf Harroch <carl@novoda.com>
2010-04-18 13:32:50 +0100

    Moving to maven as buildr was failing

--- RESTProvider/src/test/java/novoda/rest/cursors/json/JsonCursorTest.java
+++ RESTProvider/src/test/java/novoda/rest/cursors/json/JsonCursorTest.java
@@ -3,15 +3,15 @@ package novoda.rest.cursors.json;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.when;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class JsonCursorTest {
