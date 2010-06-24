4d2fa35392833b7c054b527e9b65f93291496439
Carl-Gustaf Harroch <carl@novoda.com>
2010-06-24 01:53:40 +0100

    minor changes

--- RESTProvider/src/main/java/novoda/rest/UriRequestMap.java
+++ RESTProvider/src/main/java/novoda/rest/UriRequestMap.java
@@ -1,8 +1,8 @@
-
 package novoda.rest;
 
-import java.util.Map;
+import java.util.List;
 
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpUriRequest;
 
@@ -11,27 +11,32 @@ import android.net.Uri;
 
 public abstract class UriRequestMap {
 
-    static final public int QUERY = 0;
+	static final public int QUERY = 0;
 
-    static final public int INSERT = 1;
+	static final public int INSERT = 1;
 
-    static final public int UPDATE = 2;
+	static final public int UPDATE = 2;
 
-    static final public int DELETE = 3;
+	static final public int DELETE = 3;
 
-    /**
-     * @param uri
-     * @param type
-     * @param params
-     * @return
-     */
-    public abstract HttpUriRequest getRequest(Uri uri, int type, Map<String, String> params);
+	/**
+	 * @param uri
+	 * @param type
+	 * @param params
+	 * @return
+	 */
+	public abstract HttpUriRequest getRequest(Uri uri, int type,
+			List<NameValuePair> params);
 
-    public abstract ResponseHandler<? extends AbstractCursor> getQueryHandler(Uri uri);
+	public abstract ResponseHandler<? extends AbstractCursor> getQueryHandler(
+			Uri uri);
 
-    public abstract ResponseHandler<? extends Uri> getInsertHandler(Uri uri);
+	public abstract ResponseHandler<? extends Uri> getInsertHandler(Uri uri);
 
-    public abstract ResponseHandler<? extends Integer> getUpdateHandler(Uri uri);
+	public abstract ResponseHandler<? extends Integer> getUpdateHandler(Uri uri);
 
-    public abstract ResponseHandler<? extends Integer> getDeleteHandler(Uri uri);
+	public abstract ResponseHandler<? extends Integer> getDeleteHandler(Uri uri);
+
+	public abstract List<NameValuePair> tranform(String[] projection,
+			String selection, String[] selectionArg, String sortOrder);
 }
