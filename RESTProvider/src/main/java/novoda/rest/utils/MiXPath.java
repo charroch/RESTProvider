5de2fa0e58605e07daf9647a162fa0174b14687a
Carl Harroch <carl@novoda.com>
2010-07-29 18:57:54 +0100

    working on a minimal XPath implementation for easier access to nodes/values within a JSON/XML

--- RESTProvider/src/main/java/novoda/rest/utils/MiXPath.java
+++ RESTProvider/src/main/java/novoda/rest/utils/MiXPath.java
@@ -1,5 +1,18 @@
+
 package novoda.rest.utils;
 
-public class MiXPath {
+public abstract class MiXPath<T> {
+    
+    char bracket_o = '[';
 
+    char bracket_c = ']';
+
+    char separator = '/';
+
+    public abstract T eventArray(T data, int index);
+
+    public abstract T eventPath(T data, String path);
+    
+    public void parse(String parser) {
+    }
 }
